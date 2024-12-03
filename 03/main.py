02af5bcf3305fb46e1da0f3bec5b0a73c4bba25a
tokky01 <war.verlinde@gmail.com>
2024-12-03 09:22:35 +0100

    more elgant solution for pt 2

--- 03/main.py
+++ 03/main.py
@@ -5,39 +5,23 @@ def read_file():
     with open("input.txt", encoding='UTF8') as f:
         return f.read()
 
-
-def do_dont_mul(memory):
-    mul_pattern = r"mul\(\s*(-?\d+)\s*,\s*(-?\d+)\s*\)"
-    do_pattern = r"do\(\)"
-    dont_pattern = r"don't\(\)"
-
-    multiply = True
-    total = 0
-
-    for pos in range(len(memory)):
-
-        do_match = re.match(do_pattern, memory[pos:])
-        if do_match:
-            multiply = True
-            pos += do_match.end()
-            continue
-
-        dont_match = re.match(dont_pattern, memory[pos:])
-        if dont_match:
-            multiply = False
-            pos += dont_match.end()
-            continue
-
-        mul_match = re.match(mul_pattern, memory[pos:])
-        if mul_match:
-            if multiply:
-                x, y = int(mul_match.group(1)), int(mul_match.group(2))
-                total += x * y
-            pos += mul_match.end()
-            continue
-
-    return total
-
 file = read_file()
-print(do_dont_mul(file))
+
+regex = "(mul\((\d+),(\d+)\)|do\(\)|don't)"
+
+matches = re.findall(regex, file)
+
+res = 0
+enabled = True
+for match in matches:
+    print(match)
+    if match[0] == "do()":
+        enabled = True
+    elif match[0] == "don't":
+        enabled = False
+    elif enabled:
+        res += int(match[1]) * int(match[2])
+
+print(res)
+
 
